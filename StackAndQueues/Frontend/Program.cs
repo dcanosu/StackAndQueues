ca8c3eefdef5071a20a35e040c5d7166ec9e6b8c
dcanosua <dcanosua+github@cisco.com>
2026-03-29 12:48:39 -0500

    feat: Implement a menu

--- StackAndQueues/Frontend/Program.cs
+++ StackAndQueues/Frontend/Program.cs
@@ -1,20 +1,77 @@
-﻿using Backend;
+﻿using System.ComponentModel.Design;
+using Backend;
 
-try
-{
+// try
+// {
     var stack = new StackUsingArray<string>(5);
-    stack.Push("Perro");
-    stack.Push("Gato");
-    stack.Push("Pájaro");
-    stack.Push("Pez");
-    stack.Push("Conejo");
-    for (int i = 0; i < 10; i++)
-    {
-        Console.WriteLine(stack.Pop());
-    }
-}
-catch (Exception ex)
-{
+//     stack.Push("Perro");
+//     stack.Push("Gato");
+//     stack.Push("Pájaro");
+//     stack.Push("Pez");
+//     stack.Push("Conejo");
+//     for (int i = 0; i < 10; i++)
+//     {
+//         Console.WriteLine(stack.Pop());
+//     }
+// }
+// catch (Exception ex)
+// {
     
-    Console.WriteLine("An error occurred: " + ex.Message);
-}
+//     Console.WriteLine("An error occurred: " + ex.Message);
+// }
+
+
+    var option = string.Empty;
+
+    do
+    {
+        option = Menu();
+        switch (option)
+        {
+            case "1":
+                Console.Write("Ingrese el elemento a apilar: ");
+                var element = Console.ReadLine() ?? string.Empty;
+                stack.Push(element);
+                Console.WriteLine($"Elemento '{element}' apilado.");
+                break;
+            case "2":
+                try
+                {
+                    var poppedElement = stack.Pop();
+                    Console.WriteLine($"Elemento '{poppedElement}' desapilado.");
+                }
+                catch (InvalidOperationException ex)
+                {
+                    Console.WriteLine("Error: " + ex.Message);
+                }
+                break;
+            case "3":
+                try
+                {
+                    var topElement = stack.Peek();
+                    Console.WriteLine($"El tope de la pila es: '{topElement}'.");
+                }
+                catch (InvalidOperationException ex)
+                {
+                    Console.WriteLine("Error: " + ex.Message);
+                }
+                break;
+            case "0":
+                Console.WriteLine("Saliendo del programa...");
+                break;
+            default:
+                Console.WriteLine("Opción no válida. Por favor, intente de nuevo.");
+                break;
+        }
+    } while (option != "0");
+
+    string Menu()
+    {
+        Console.WriteLine("Select an option:");
+        Console.WriteLine("1. Apilar");
+        Console.WriteLine("2. Desapilar");
+        Console.WriteLine("3. Ver tope");
+        Console.WriteLine("0. Salir");
+        Console.Write("Digite su opción: ");
+        return Console.ReadLine() ?? string.Empty;
+    }
