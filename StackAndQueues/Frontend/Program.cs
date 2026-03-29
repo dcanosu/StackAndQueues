5d7e254981f76f65ae218042f599c2bfac86593b
dcanosua <dcanosua+github@cisco.com>
2026-03-29 12:32:21 -0500

    feat: Implement an arry using stack

---
+++ StackAndQueues/Frontend/Program.cs
@@ -0,0 +1,20 @@
+﻿using Backend;
+
+try
+{
+    var stack = new StackUsingArray<string>(5);
+    stack.Push("Perro");
+    stack.Push("Gato");
+    stack.Push("Pájaro");
+    stack.Push("Pez");
+    stack.Push("Conejo");
+    for (int i = 0; i < 10; i++)
+    {
+        Console.WriteLine(stack.Pop());
+    }
+}
+catch (Exception ex)
+{
+    
+    Console.WriteLine("An error occurred: " + ex.Message);
+}
